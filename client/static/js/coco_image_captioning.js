69189d419f1d8d9ac5981fc156f0c6fd1e9bd55e
Thanh Nguyen <thanhnp@atvn.com.vn>
2017-06-14 16:01:37 +0700

    Clear show dialog

--- client/static/js/coco_image_captioning.js
+++ client/static/js/coco_image_captioning.js
@@ -1,13 +1,13 @@
 // ============================================================================
 // create question set and state to go through the questions
 // ============================================================================
-var questions = new Array();
+var url = "";
+var next_url = "coco_process_step_2.html";
+var questions = [];
 var state = 0;
 var init_time = $.now();
 // TODO: define list of image urls to load in this page
-var im_urls = ["http://orig13.deviantart.net/ac5c/f/2011/345/8/0/business_cat_original_by_plasticpyre-d4irr6a.jpg",
-  "http://orig03.deviantart.net/0336/f/2012/204/d/f/cute_cat_by_animexchibixmanga-d58ajxe.jpg",
-  "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-Mks10gAfyzFUEFgdISlD-2N-ymFK3KiirV1tl_gEyLTVy0ju"];
+var im_urls = ["http://orig13.deviantart.net/ac5c/f/2011/345/8/0/business_cat_original_by_plasticpyre-d4irr6a.jpg"];
 
 // ============================================================================
 // initialize images
@@ -26,25 +26,15 @@ for (i = 0; i < im_urls.length; i++) {
 // page onload
 // ============================================================================
 $(window).load(function () {
+
   render_question(questions[0]);
-  $("#dialog-modal").dialog({
-    autoOpen: false,
-    height: 250,
-    modal: true,
-    buttons: {
-      'ok': function () {
-        $(this).dialog("close");
-      }
-    }
-  });
-  addDialog();
-  $("#dialog-modal").hide();
+
   $('#next').bind('click', function () {
     // Goto Step 2
-    window.location.replace("coco_process_step_2.html");
+    window.location.replace(next_url);
   });
   $('#submit').bind('click', function () {
-    next();
+    submit();
   });
   $('#clear').bind('click', function () {
     $('#description').val("");
@@ -54,7 +44,8 @@ $(window).load(function () {
 // ================================================================
 // function to control next and previous question
 // ===============================================================
-function next() {
+
+function submit() {
   var q = questions[state];
   // store user input
   q.ans = $('#description').val();
@@ -62,19 +53,24 @@ function next() {
     render_dialog(7);
     return -1;
   }
-  if (state < questions.length - 1) {
-    state += 1;
-  } else {
-    $("#dialog-confirm").dialog('open');
-  }
-  render_question(questions[state]);
+
+  var data = {
+    image_id: 1,
+    userId: 1,
+    caption: q.ans
+  };
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data
+  }).success(function (result, status, xhr) {
+    console.log("Done");
+  }).error(function (xhr, status, error) {
+    console.log(data);
+  });
 }
-function prev() {
-  if (state > 0) {
-    state -= 1;
-  }
-  render_question(questions[state]);
-}
+
 // ===============================================================
 // rednering question, image, and dialog
 // ==============================================================
@@ -87,6 +83,7 @@ function render_question(q) {
   $('#description').val(q.ans);
   $('#header').text('Please describe the image:');
 }
+
 function render_im(im) {
   im.height = im.height * 480 / im.width;
   im.width = 480;
@@ -101,48 +98,32 @@ function render_im(im) {
   var ctx = c.getContext("2d");
   ctx.drawImage(im, 0, 0, c.width, c.height);
 }
-function render_dialog(idx) {
-  if (idx === 1) {
-    var text = 'Do not describe unimportant details.';
-  } else if (idx === 2) {
-    var text = 'Do not describe things that might have happened in the future or past.';
-  } else if (idx === 3) {
-    var text = 'Do not describe what a person might say.';
-  } else if (idx === 4) {
-    var text = 'Do not give people proper names.';
-  } else if (idx === 5) {
-    var text = 'This is the BEST sentence! Please apply the same rule to describe following 3 images.';
-  } else if (idx === 6) {
-    var text = 'Please includes more details.';
-  } else if (idx === 7) {
-    var text = 'Please enter more than 8 words.';
-  } else if (idx === 8) {
-    var text = 'Error occured when submitting the form, please try again.';
-  }
-  $("#dialog-modal").text(text);
-  $("#dialog-modal").dialog('open');
-}
 
-// ===========================================================
-// add dialog for the web page
-// ===========================================================
-function addDialog() {
-  $("#dialog-confirm").dialog({
-    autoOpen: false,
-    resizable: false,
-    height: 140,
-    modal: true,
-    buttons: {
-      "Yes": function () {
-        $(this).dialog("close");
-        submit_form();
-      },
-      Cancel: function () {
-        $(this).dialog("close");
-      }
-    }
+function render_dialog(idx) {
+  var text = "";
+  if (idx === 1) {
+    text = 'Do not describe unimportant details.';
+  } else if (idx === 2) {
+    text = 'Do not describe things that might have happened in the future or past.';
+  } else if (idx === 3) {
+    text = 'Do not describe what a person might say.';
+  } else if (idx === 4) {
+    text = 'Do not give people proper names.';
+  } else if (idx === 5) {
+    text = 'This is the BEST sentence! Please apply the same rule to describe following 3 images.';
+  } else if (idx === 6) {
+    text = 'Please includes more details.';
+  } else if (idx === 7) {
+    text = 'Please enter more than 8 words.';
+  } else if (idx === 8) {
+    text = 'Error occured when submitting the form, please try again.';
+  }
+
+  BootstrapDialog.show({
+    type: 'type-danger',
+    title: 'ERROR',
+    message : text
   });
-  $(".ui-dialog").css('position', 'absolute');
 }
 
 // ===========================================================
